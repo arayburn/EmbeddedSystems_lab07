007677e3b72ca2e2994c75291f15cfb2e31ca7f5
Alex Rayburn <arayburn2002@gmail.com>
2024-03-27 08:21:28 -0600

    update

---
+++ EmbeddedSystemsLab07Code/MDK-ARM/RTE/_EmbeddedSystemsLab07Code/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'EmbeddedSystemsLab07Code' 
+ * Target:  'EmbeddedSystemsLab07Code' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
